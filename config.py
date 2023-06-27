93afd1de57ba9bacda248214b8a39e4b3a3f1881
herokutech <114892688+herokutech@users.noreply.github.com>
2023-06-27 20:51:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import getenv
 
 class Config(object):
-      API_HASH = getenv("API_HASH")
-      API_ID = int(getenv("API_ID", 0))      
-      BOT_TOKEN = getenv("BOT_TOKEN", "")
-      CHANNEL = list(x for x in getenv("CHANNEL_ID", "").replace("\n", " ").split(' '))
+      API_HASH = getenv("API_HASH" "670deb1f30b648fbb4f744e406161942")
+      API_ID = int(getenv("API_ID", "13984519"))      
+      BOT_TOKEN = getenv("BOT_TOKEN", "6210597044:AAHMJw2jiROeZ9F0ZI26LX1QjgTD7IvONfM")
+      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1001612590180").replace("\n"," -1001807370305").split(' '))
